79638d4a2e1ebac24294131aaba4b33aa30af8a8
Boots2291 <buteramv@gmail.com>
2017-12-22 11:14:55 -0700

    Break into modules

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,71 +1,21 @@
 require 'pry'
-require_relative 'alphabet'
 require_relative 'converter'
+require_relative 'lookup'
 
 class NightWriter
-  include Converter
-  attr_reader :alphabet
+  include Lookup
+  attr_reader :converter
 
   def initialize
-    @alphabet = Alphabet.new
-  end
-
-  def lookup(character, position)
-    @alphabet.braille_letter_hash[character].chars[position]
+    @converter = Converter.new
   end
 
   def encode_to_braille(plain)
-    output = []
-    [0,2,4].each do |offset|
-      encode_letter(plain, offset, output)
-      output << "\n"
-    end
-    encoded = output.join
-  end
-
-  def encode_letter(plain, offset, output)
-    plain.chars.each do |letter|
-      handle_case(letter, output, offset)
-    end
-  end
-
-  def handle_case(letter, output, offset)
-    if letter == letter.upcase
-      output << lookup(:capitalize, offset) << lookup(:capitalize, offset + 1)
-      letter = letter.downcase
-    end
-    output << lookup(letter, offset) << lookup(letter, offset + 1)
+    @converter.encode_to_braille(plain)
   end
 
   def encode_from_braille(braille)
-    lines = braille.split("\n")
-    n = lines[0].length
-    m = 3
-    as_one_line = lines.join
-    output = []
-    should_capitalize_next = false
-
-    (0..(n-1)).each_slice(2) do |column_offset|
-
-      braille_character = []
-      (0..(m-1)).each do |row_offset|
-        braille_character << as_one_line[(row_offset * n) + column_offset[0]]
-        braille_character << as_one_line[(row_offset * n) + column_offset[1]]
-      end
-
-      decoded_braille = @alphabet.braille_letter_hash.key(braille_character.join)
-
-      if decoded_braille == :capitalize
-        should_capitalize_next = true
-      elsif should_capitalize_next
-        output << decoded_braille.upcase
-        should_capitalize_next = false
-      else
-        output << decoded_braille
-        should_capitalize_next = false
-      end
-    end
-    output.join
+    @converter.from_braille(braille)
   end
 end
 
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,22 +1,25 @@
 require './test/test_helper'
 require './lib/night_writer'
 require './lib/alphabet'
-
+require './lib/converter'
+require './lib/lookup'
+require 'pry'
 class NightWriterTest < Minitest::Test
 
   def writer
     @writer ||= NightWriter.new
   end
+
   def test_can_lookup
-    assert_equal ".", writer.lookup("a", 4)
+    assert_equal ".", Lookup.lookup("a", 4)
   end
 
   def test_postion
-    assert_equal ".", writer.lookup(:capitalize, 0)
+    assert_equal ".", Lookup.lookup(:capitalize, 0)
   end
 
   def test_can_encode_a_letter
-    assert_equal "0.\n..\n..\n", writer.encode_to_braille("a")
+    assert_equal "0.\n..\n..\n", writer.converter.encode_to_braille("a")
   end
 
   def test_can_encode_cap_letter
