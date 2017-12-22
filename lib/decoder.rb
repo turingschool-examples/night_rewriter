6e1bcc9b5629009e2376599eaf788af5925fc8b5
jayphodges <jayphodges@gmail.com>
2017-12-22 11:50:59 -0700

    Initial decoder refactor completed

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -8,6 +8,31 @@ class Decoder
     @alphabet = Alphabet.new
   end
 
+  def decode(braille)
+    lines = braille.split("\n")
+    output = []
+    (lines[0].length / 2).times do
+      letter = lines[0].slice!(0,2) + lines[1].slice!(0,2) + lines[2].slice!(0,2)
+      output << @alphabet.braille_letter_hash.key(letter)
+    end
+    format_braille(output)
+  end
+
+  def format_braille(array)
+    caps = false
+    output = []
+    array.each do |letter|
+      if letter == :capitalize
+        caps = true
+      elsif caps
+        output << letter.upcase
+        caps = false
+      else
+        output << letter
+      end
+    end
+    output.reduce(:+)
+  end
 
 
 end
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,12 +1,13 @@
 require_relative 'alphabet'
 require_relative 'encoder'
+require_relative 'decoder'
 require 'pry'
 
 class NightWriter
 
   def initialize
-    @alphabet = Alphabet.new
     @encoder = Encoder.new
+    @decoder = Decoder.new
   end
 
   def encode_to_braille(plain)
@@ -18,48 +19,7 @@ class NightWriter
   end
 
   def encode_from_braille(braille)
-    lines = braille.split("\n")
-    n = lines[0].length
-    m = 3
-    as_one_line = lines.join
-    output = []
-    should_capitalize_next = false
-
-    (0..(n-1)).each_slice(2) do |column_offset|
-
-      braille_character = []
-      (0..(m-1)).each do |row_offset|
-        braille_character << as_one_line[(row_offset * n) + column_offset[0]]
-        braille_character << as_one_line[(row_offset * n) + column_offset[1]]
-      end
-
-      decoded_braille = @alphabet.braille_letter_hash.key(braille_character.join)
-
-      if decoded_braille == :capitalize
-        should_capitalize_next = true
-      elsif should_capitalize_next
-        output << decoded_braille.upcase
-        should_capitalize_next = false
-      else
-        output << decoded_braille
-        should_capitalize_next = false
-      end
-    end
-    output.join
+    @decoder.decode(braille)
   end
-
-  def decode(braille)
-    lines = braille.split("\n")
-  end
-
 
 end
-# nw = NightWriter.new
-# thing1 = nw.encode_to_braille('A')
-# thing2 = nw.encode_to_braille('a')
-# print thing1
-# print "\n"
-# print thing2
-# thing2 = nw.encode_to_braille('Todd')
-
-# print thing2
