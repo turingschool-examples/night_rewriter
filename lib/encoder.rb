40e2c15f9df68903ae1c1c3249cd860d5046d85c
Aaron Hursh <amhursh@gmail.com>
2017-12-22 10:51:09 -0700

    Create encoder class

---
+++ lib/encoder.rb
@@ -0,0 +1,50 @@
+require_relative 'alphabet'
+
+class Encoder
+
+  attr_reader :input,
+              :output
+
+  def initialize(input)
+    @input = input
+    @output = []
+    @alphabet = Alphabet.new
+  end
+
+  def encode
+    build_lines(input)
+    output.join
+  end
+
+  def build_lines(plain)
+    [0,2,4].each do |offset|
+      convert(plain, offset)
+    end
+  end
+
+  def convert(plain, offset)
+    append_line(plain, offset)
+    append_line_break
+  end
+
+  def append_line_break
+    output << "\n"
+  end
+
+  def append_line(plain, offset)
+    plain.chars.each do |letter|
+      letter = capitalize(letter, offset) if letter == letter.upcase
+      append_braille_letter(letter, offset)
+    end
+  end
+
+  def append_braille_letter(letter, offsetg)
+    output << @alphabet.lookup(letter, offset) << @alphabet.lookup(letter, offset + 1)
+  end
+
+  def capitalize(letter, offset)
+    output << @alphabet.lookup(:capitalize, offset) << @alphabet.lookup(:capitalize, offset + 1)
+    letter.downcase
+  end
+
+end
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,4 +1,5 @@
 require_relative 'alphabet'
+require_relative 'encoder'
 
 class NightWriter
 
@@ -9,42 +10,8 @@ class NightWriter
   # MASTER METHOD - ENCODE
 
   def encode_to_braille(plain)
-    output = []
-    build_lines(plain, output)
-    output.join
-  end
-
-  # encode to braille
-
-  def build_lines(plain, output)
-    [0,2,4].each do |offset|
-      convert(plain, offset, output)
-    end
-  end
-
-  def convert(plain, offset, output)
-    append_line(plain, offset, output)
-    append_line_break(output)
-  end
-
-  def append_line_break(output)
-    output << "\n"
-  end
-
-  def append_line(plain, offset, output)
-    plain.chars.each do |letter|
-      letter = capitalize(letter, offset, output) if letter == letter.upcase
-      append_braille_letter(letter, offset, output)
-    end
-  end
-
-  def append_braille_letter(letter, offset, output)
-    output << @alphabet.lookup(letter, offset) << @alphabet.lookup(letter, offset + 1)
-  end
-
-  def capitalize(letter, offset, output)
-    output << @alphabet.lookup(:capitalize, offset) << @alphabet.lookup(:capitalize, offset + 1)
-    letter.downcase
+    encoder = Encoder.new(plain)
+    encoder.encode
   end
 
   # MASTER METHOD - DECODE
