f63d2b669d89c85203f0573d20a93db993e8b05b
Ricky Amparo <rickyamparo@gmail.com>
2017-12-22 09:44:01 -0700

    change the output to an attribute of the encoder class

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -7,21 +7,21 @@ class Encoder
 
   def initialize
     @alphabet = Alphabet.new
+    @output =[]
   end
 
   def encode_to_braille(plain)
-    output = []
     [0,2,4].each do |offset|
       plain.chars.each do |letter|
         if letter == letter.upcase
-          output << lookup(:capitalize, offset) << lookup(:capitalize, offset + 1)
+          @output << lookup(:capitalize, offset) << lookup(:capitalize, offset + 1)
           letter = letter.downcase
         end
-        output << lookup(letter, offset) << lookup(letter, offset + 1)
+        @output << lookup(letter, offset) << lookup(letter, offset + 1)
       end
-      output << "\n"
+      @output << "\n"
     end
-    encoded = output.join
+    encoded = @output.join
   end
 
 end
