4ceac3dd07767159b3d2242b872b1654bfce75e7
Becki Srofe <blf_23@yahoo.com>
2017-12-22 10:27:11 -0700

    Pull out convert method from encode_to_braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -11,18 +11,24 @@ class NightWriter
   end
 
   def encode_to_braille(plain)
-    output = []
-    [0,2,4].each do |offset|
-      plain.chars.each do |letter|
-        if letter == letter.upcase
-          output << lookup(:capitalize, offset) << lookup(:capitalize, offset + 1)
-          letter = letter.downcase
-        end
-        output << lookup(letter, offset) << lookup(letter, offset + 1)
-      end
+
+    new_output = [0,2,4].map do |offset|
+      output = convert(plain, offset)
       output << "\n"
     end
-    encoded = output.join
+    encoded = new_output.join
+  end
+
+  def convert(plain, offset)
+    output =[]
+    plain.chars.each do |letter|
+      if letter == letter.upcase
+        output << lookup(:capitalize, offset) << lookup(:capitalize, offset + 1)
+        letter = letter.downcase
+      end
+      output << lookup(letter, offset) << lookup(letter, offset + 1)
+    end
+    return output
   end
 
   def encode_from_braille(braille)
@@ -56,4 +62,3 @@ class NightWriter
     output.join
   end
 end
-
