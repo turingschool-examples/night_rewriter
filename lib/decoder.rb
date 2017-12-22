0a50fbcbc1ac50610e1fc65897427623551532e1
Ricky Amparo <rickyamparo@gmail.com>
2017-12-22 10:43:51 -0700

    extract capitalization check into its own method

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -17,26 +17,27 @@ class Decoder
     n = @lines[0].length
 
     (0..(n-1)).each_slice(2) do |column_offset|
-
       braille_character = []
       (0..2).each do |row_offset|
         braille_character << @lines.join[(row_offset * n) + column_offset[0]]
         braille_character << @lines.join[(row_offset * n) + column_offset[1]]
       end
-
       decoded_braille = @alphabet.braille_letter_hash.key(braille_character.join)
-
-      if decoded_braille == :capitalize
-        @should_capitalize_next = true
-      elsif @should_capitalize_next
-        @output << decoded_braille.upcase
-        @should_capitalize_next = false
-      else
-        @output << decoded_braille
-        @should_capitalize_next = false
-      end
+      capital_check(decoded_braille)
     end
     @output.join
+  end
+
+  def capital_check(decoded_braille)
+    if decoded_braille == :capitalize
+      @should_capitalize_next = true
+    elsif @should_capitalize_next
+      @output << decoded_braille.upcase
+      @should_capitalize_next = false
+    else
+      @output << decoded_braille
+      @should_capitalize_next = false
+    end
   end
 
 end
