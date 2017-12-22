1fdc7b9df3e7522ef78d202b9ca4d899098d2a90
Aaron Hursh <amhursh@gmail.com>
2017-12-22 09:26:17 -0700

    Move lookup method to alphabet class. fix tests

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,19 +6,19 @@ class NightWriter
     @alphabet = Alphabet.new
   end
 
-  def lookup(character, position)
-    @alphabet.braille_letter_hash[character].chars[position]
-  end
+  # def lookup(character, position)
+  #   @alphabet.braille_letter_hash[character].chars[position]
+  # end
 
   def encode_to_braille(plain)
     output = []
     [0,2,4].each do |offset|
       plain.chars.each do |letter|
         if letter == letter.upcase
-          output << lookup(:capitalize, offset) << lookup(:capitalize, offset + 1)
+          output << @alphabet.lookup(:capitalize, offset) << @alphabet.lookup(:capitalize, offset + 1)
           letter = letter.downcase
         end
-        output << lookup(letter, offset) << lookup(letter, offset + 1)
+        output << @alphabet.lookup(letter, offset) << @alphabet.lookup(letter, offset + 1)
       end
       output << "\n"
     end
